f74f8747f65b8125c4d69ff42fce4ea774ab9749
BCPTe <56164897+BCPTe@users.noreply.github.com>
2023-05-20 20:54:19 +0200

    Added popover and card flip

--- src/components/Survey/Survey.js
+++ src/components/Survey/Survey.js
@@ -1,12 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import React, { useEffect, useState } from 'react'
-import { Button, Col } from 'react-bootstrap'
+import React, { useEffect, useRef, useState } from 'react'
+import { Button, Col, Row, OverlayTrigger, Overlay, Popover } from 'react-bootstrap'
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import api from '../../API/axiosConfig'
 import { useAuth } from '../../Contexts/AuthContext'
 import './Survey.css'
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faTimes, faUser } from "@fortawesome/free-solid-svg-icons";
+import ReactCardFlip from "react-card-flip"
 
 
 const Survey = () => {
@@ -14,7 +15,8 @@ const Survey = () => {
 	const auth = authStatus()
 
 	const [date, setDate] = useState(new Date())
-	const [datesInDb, setDatesInDb] = useState([])
+	const [datesInDb, setDatesInDb] = useState([{ date: "2000-4-27" }])
+	const [playersInDay, setPlayersInDay] = useState([{ name: auth.authUser.name, surname: auth.authUser.surname, username: auth.authUser.username }])
 
 	const printDate = (dateToPrint) => {
 		const temp = new Date(dateToPrint)
@@ -36,47 +38,105 @@ const Survey = () => {
 			date: date
 		}
 
-		api.post("", payload)
+		// TODO: IMPLEMENT IN BACKEND
+		api.post("/api/v1/surveys/add", payload)
 	}
+
+	useEffect(() => {
+		const payload = {
+			date: date
+		}
+
+		// TODO: IMPLEMENT IN BACKEND
+		api.get("/api/v1/surveys", {
+			params: { date: date }
+		})
+			.then(response => {
+				setPlayersInDay(response.data)
+			})
+	}, [date])
+
 
 	useEffect(() => {
 		getUserDates()
 		// console.warn("ECCO LE DATE NEL DB: ", datesInDb?.length)
 	}, [])
 
-	// useEffect(() => {
-	// 	// getUserDates()
-	// 	console.warn("ECCO LE DATE NEL DB: ", datesInDb)
-	// }, [datesInDb])
+	// for flipcard
+	const [flip, setFlip] = useState(false)
+	//
+
+
+	const handleRemoveAvailability = () => {
+		// TODO: IMPLEMENT IN BACKEND
+	}
+	//
+
 
 	return (
 		<div className='survey-container'>
-			<Col xs={10} md={6} lg={4} className='min-50'>
-				<div className='survey-form'>
-					{datesInDb?.length < 2 &&
-						<>
-							<Calendar className={"custom-calendar"} onChange={setDate} value={date} />
-							<div className='calendar-btns'>
-								<Button variant='secondary' onClick={handleAddDate}>Add</Button>
+			<Col xs={10} md={5} lg={4} className='h-75'>
+				<ReactCardFlip isFlipped={flip}>
+					<div className='survey-form'>
+						{datesInDb?.length < 2 &&
+							<>
+								<Calendar className="custom-calendar" onChange={setDate} value={date} />
+								<div className='calendar-btns'>
+									<Button variant='secondary' onClick={handleAddDate}>Add</Button>
+									<Button variant='outline-primary' onClick={() => setFlip(!flip)}>View {playersInDay.length} players</Button>
+								</div>
+							</>
+						}
+						{datesInDb.length > 0 &&
+							<div className="d-flex w-100 flex-column">
+								<div className="fw-bold text-center mt-1 mb-1">Date che hai selezionato:</div>
+								<div className='dates-in-db'>
+									{datesInDb?.map((item, index) =>
+										<div className="date" key={index}>
+											<div>{printDate(item.date)}</div>
+											<OverlayTrigger
+												rootClose
+												trigger="click"
+												placement="right"
+												overlay={
+													<Popover className="popover_del_component">
+														<Popover.Body>
+															<div className="text-center">Do you really want to remove your availability for {item.date}?</div>
+															<div className="d-flex justify-content-around mt-3">
+																<Button variant="danger" onClick={handleRemoveAvailability}>Yes</Button>
+																<Button variant="secondary" onClick={() => document.body.click()}>No</Button>
+															</div>
+														</Popover.Body>
+													</Popover>}>
+												<Button variant='danger' date-id={item.id}>
+													<FontAwesomeIcon icon={faTimes} />
+												</Button>
+											</OverlayTrigger>
+										</div>
+									)}
+								</div>
 							</div>
-						</>
-					}
-					{datesInDb.length > 0 &&
-						<div className="d-flex w-100 flex-column">
-							<div className="fw-bold text-center mt-3 mb-1">Date che hai selezionato:</div>
-							<div className='dates-in-db'>
-								{datesInDb?.map((item, index) =>
-									<div className="date" key={index}>
-										<div>{printDate(item.date)}</div>
-										<Button variant='danger' date-id={item.id}>
-											<FontAwesomeIcon icon={faTimes} />
-										</Button>
-									</div>
-								)}
-							</div>
+						}
+					</div>
+					<div className='survey-form'>
+						<div className="players-in-day">
+							{playersInDay.map((player, index) =>
+								<Row className="player" key={index}>
+									<Col sm={8}>
+										<FontAwesomeIcon icon={faUser} />
+										<span className="name-surname">{player.name} {player.surname}</span>
+									</Col>
+									<Col sm={4} className="text-end">
+										<span className="username fw-bold">{player.username}</span>
+									</Col>
+								</Row>
+							)}
 						</div>
-					}
-				</div>
+						<div className="down-btns">
+							<Button variant='outline-primary' onClick={() => setFlip(!flip)}>Return to calendar</Button>
+						</div>
+					</div>
+				</ReactCardFlip>
 			</Col>
 		</div>
 
